abeddb6c7160f66eb02a7efb354811bf6812df41
Maria Tudose <maria.tudose@aalto.fi>
2019-11-27 19:19:58 +0200

    added redux store

--- src/routes.js
+++ src/routes.js
@@ -5,23 +5,32 @@ import {
   Route,
 } from "react-router-dom";
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './store/reducers'
+
 import BaseLayout from './components/BaseLayout'
 import NewsItem from './components/NewsItem'
 import NewsList from './components/NewsList'
 import './index.scss';
 
+
+const store = createStore(rootReducer)
+
 class Routes extends React.Component {
   render() {
     return (
-      <Router>
-        <BaseLayout>
-          <Switch>
-            <Route path="/:articleId" component={NewsItem} />
-            <Route path="/" component={NewsList} />
-          </Switch>
+      <Provider store={store}>
+        <Router>
+          <BaseLayout>
+            <Switch>
+              <Route path="/:articleId" component={NewsItem} />
+              <Route path="/" component={NewsList} />
+            </Switch>
 
-        </BaseLayout>
-      </Router>
+          </BaseLayout>
+        </Router>
+      </Provider>
     )
   }
 }
