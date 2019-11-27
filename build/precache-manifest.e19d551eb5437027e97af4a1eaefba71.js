69e3c5df06771146964d4cc59dc902246dbdf693
Levänen Linda <levanel2@cs.hut.fi>
2019-11-27 19:23:06 +0200

    Added the build folder to git

---
+++ build/precache-manifest.e19d551eb5437027e97af4a1eaefba71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f76fd3e8ab89c0bd59582c23d444e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da7cab911f5d481744ed",
+    "url": "/static/css/main.310c605b.chunk.css"
+  },
+  {
+    "revision": "6fff08361ef55d5000b3",
+    "url": "/static/js/2.c988bda3.chunk.js"
+  },
+  {
+    "revision": "da7cab911f5d481744ed",
+    "url": "/static/js/main.3ae3d86f.chunk.js"
+  },
+  {
+    "revision": "074a32bdf3a8db21e010",
+    "url": "/static/js/runtime-main.64cd4850.js"
+  },
+  {
+    "revision": "ebced14106f874979db9a35d284bc6da",
+    "url": "/static/media/built_titling.ebced141.ttf"
+  }
+]);
